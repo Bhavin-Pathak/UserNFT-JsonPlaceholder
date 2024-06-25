e7c3465eec06dec04685f1b80a89a4debf1f4550
Bhavin8289 <bupadhyay277@gmail.com>
2024-06-25 09:34:03 +0530

    Initial Commit

---
+++ Prodev/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Prodev
+//
+//  Created by MacBookPro on 04/04/1946 Saka.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Prodev/ProdevApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProdevApp.swift
+//  Prodev
+//
+//  Created by MacBookPro on 04/04/1946 Saka.
+//
+
+import SwiftUI
+
+@main
+struct ProdevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
